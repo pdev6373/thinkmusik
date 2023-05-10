9e729333b1c28013b7773ee0e33314c2432f15be
pdev6373 <pdev6373@gmail.com>
2023-05-10 10:07:06 +0100

    first commit

--- components/frequentlyAskedQuestions/FrequentlyAskedQuestions.tsx
+++ components/frequentlyAskedQuestions/FrequentlyAskedQuestions.tsx
@@ -9,26 +9,72 @@ const myFont = localFont({ src: "../../fonts/GTWalsheimPro-Bold.woff2" });
 const myFont2 = localFont({ src: "../../fonts/GTWalsheimPro-Medium.woff2" });
 
 const allQuestions = [
-  "Is Thinkmusik right for me?",
-  "What makes Pickup Music better than other guitar lesson sites?",
-  "What's included in my Pickup Music membership?",
-  "How long are the lessons? How long will it take me to complete them?",
-  "I'm brand new to guitar. Is Pickup Music the right choice for me?",
+  {
+    question: "Is Thinkmusik right for me?",
+    answers: [
+      "You'll start with Pickup Music's introductory program Guitar for Complete Beginners. Over three weeks, we break down everything from simple chords and melodies to rhythm and lead playing. By the end of the program, you'll be able to play any simple pop or rock song (and even write your own).",
+      "Everything! You'll have unlimited access to every lesson on Pickup Music from every instructor. That means every Bootcamp, Master Class, SongSquad, Licks Package, 5-Minute Lesson... we could go on, but you get the idea. It's an all-inclusive membership for one affordable price.",
+      "Our lessons take you beyond the basics. Most online guitar lessons are good at teaching new guitarists the easy stuff. Pickup Music goes beyond that to help advanced-beginners and intermediate guitarists break through to the next level.",
+    ],
+  },
+  {
+    question: "What makes Pickup Music better than other guitar lesson sites?",
+    answers: [
+      "You'll start with Pickup Music's introductory program Guitar for Complete Beginners. Over three weeks, we break down everything from simple chords and melodies to rhythm and lead playing. By the end of the program, you'll be able to play any simple pop or rock song (and even write your own).",
+      "Everything! You'll have unlimited access to every lesson on Pickup Music from every instructor. That means every Bootcamp, Master Class, SongSquad, Licks Package, 5-Minute Lesson... we could go on, but you get the idea. It's an all-inclusive membership for one affordable price.",
+      "Our lessons take you beyond the basics. Most online guitar lessons are good at teaching new guitarists the easy stuff. Pickup Music goes beyond that to help advanced-beginners and intermediate guitarists break through to the next level.",
+    ],
+  },
+  {
+    question: "What's included in my Pickup Music membership?",
+    answers: [
+      "You'll start with Pickup Music's introductory program Guitar for Complete Beginners. Over three weeks, we break down everything from simple chords and melodies to rhythm and lead playing. By the end of the program, you'll be able to play any simple pop or rock song (and even write your own).",
+      "Everything! You'll have unlimited access to every lesson on Pickup Music from every instructor. That means every Bootcamp, Master Class, SongSquad, Licks Package, 5-Minute Lesson... we could go on, but you get the idea. It's an all-inclusive membership for one affordable price.",
+      "Our lessons take you beyond the basics. Most online guitar lessons are good at teaching new guitarists the easy stuff. Pickup Music goes beyond that to help advanced-beginners and intermediate guitarists break through to the next level.",
+    ],
+  },
+  {
+    question:
+      "How long are the lessons? How long will it take me to complete them?",
+    answers: [
+      "You'll start with Pickup Music's introductory program Guitar for Complete Beginners. Over three weeks, we break down everything from simple chords and melodies to rhythm and lead playing. By the end of the program, you'll be able to play any simple pop or rock song (and even write your own).",
+      "Everything! You'll have unlimited access to every lesson on Pickup Music from every instructor. That means every Bootcamp, Master Class, SongSquad, Licks Package, 5-Minute Lesson... we could go on, but you get the idea. It's an all-inclusive membership for one affordable price.",
+      "Our lessons take you beyond the basics. Most online guitar lessons are good at teaching new guitarists the easy stuff. Pickup Music goes beyond that to help advanced-beginners and intermediate guitarists break through to the next level.",
+    ],
+  },
+  {
+    question:
+      "I'm brand new to guitar. Is Pickup Music the right choice for me?",
+    answers: [
+      "You'll start with Pickup Music's introductory program Guitar for Complete Beginners. Over three weeks, we break down everything from simple chords and melodies to rhythm and lead playing. By the end of the program, you'll be able to play any simple pop or rock song (and even write your own).",
+      "Everything! You'll have unlimited access to every lesson on Pickup Music from every instructor. That means every Bootcamp, Master Class, SongSquad, Licks Package, 5-Minute Lesson... we could go on, but you get the idea. It's an all-inclusive membership for one affordable price.",
+      "Our lessons take you beyond the basics. Most online guitar lessons are good at teaching new guitarists the easy stuff. Pickup Music goes beyond that to help advanced-beginners and intermediate guitarists break through to the next level.",
+    ],
+  },
 ];
 
 const actions = ["Getting Started", "Pricing & plans", "Billing"];
 
 export default function FrequentlyAskedQuestions() {
   const [currentAction, setCurrentAction] = useState("Getting Started");
+  const [currentQuestion, setCurrentQuestion] = useState("");
 
   return (
     <section className={styles.questions}>
       <div className={styles.questionsInner}>
         <div className={styles.questionsTextWrapper}>
-          <h3 className={`${styles.questionsHeading} ${myFont.className}`}>
+          <h3
+            className={`${styles.questionsHeading} ${myFont.className}`}
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
             Frequently Asked Questions
           </h3>
-          <p className={styles.questionsBody}>
+          <p
+            className={styles.questionsBody}
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
             If your questions aren&#39;t answered below, please chat with us.
           </p>
         </div>
@@ -50,14 +96,41 @@ export default function FrequentlyAskedQuestions() {
 
         <div className={styles.questionWrapper}>
           {allQuestions.map((question) => (
-            <div className={styles.question} key={question}>
-              <p className={styles.questionText}>{question}</p>
-              <Image
-                src="/Right 24px.svg"
-                alt="dropdown"
-                width={24}
-                height={24}
+            <div
+              className={styles.questionInnerWrapper}
+              key={question.question}
+            >
+              <input
+                type="checkbox"
+                id={question.question}
+                className={styles.questionInput}
+                checked={question.question === currentQuestion}
               />
+              <label
+                htmlFor={question.question}
+                className={styles.question}
+                onClick={() =>
+                  setCurrentQuestion(
+                    currentQuestion === question.question
+                      ? ""
+                      : question.question
+                  )
+                }
+              >
+                <p className={styles.questionText}>{question.question}</p>
+                <Image
+                  src="/Right 24px.svg"
+                  alt="dropdown"
+                  width={24}
+                  height={24}
+                />
+              </label>
+
+              <div className={styles.answers}>
+                {question.answers.map((answer) => (
+                  <p className={styles.answer}>{answer}</p>
+                ))}
+              </div>
             </div>
           ))}
         </div>
